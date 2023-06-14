b5c7621dfcfc3895c0f7f320e5ff66e9b02517f8
NoobZsl <113817074+freshmanZsl@users.noreply.github.com>
2023-06-14 10:36:19 +0800

    Update c++14.cpp

--- Standard/c++14.cpp
+++ Standard/c++14.cpp
@@ -1,2 +1,38 @@
 
 /************************************************ C++ 14 ***************************************************/
+类型支持（基本类型、RTTI、类型特性）
+<type_traits>
+  检查类型是否为 std::nullptr_t
+  is_final
+<utility>
+  exchange(将实参替换为一个新值，并返回其先前值)
+  integer_sequence(实现编译时整数数列)
+<initializer_list>
+  rbegin
+  crbegin
+  rend
+  crend
+namespace
+  make_reverse_iterator(创建拥有从实参推出的类型的 std::reverse_iterator)
+<array>
+<deque>
+<forward_list>
+<iterator>
+<list>
+<map>
+<regex>
+<set>
+<span>
+<string>
+<string_view>
+<unordered_map>
+<unordered_set>
+<vector>
+  begin
+  cbegin
+  end
+  cend
+  rbegin
+  crbegin
+  rend
+  crend
